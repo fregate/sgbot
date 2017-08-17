bd5147edd8d71054370efbdef2e2270809235ea2
A <fregate@ngs.ru>
2017-08-17 11:08:15 +0700

    move bot to Service (send message before service stops with digest

--- src/sgbot/botdaemon.go
+++ src/sgbot/botdaemon.go
@@ -1,37 +1,40 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net"
-        "os"
-        "os/signal"
-        "syscall"
-        "time"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-        "github.com/takama/daemon"
+	"github.com/takama/daemon"
 )
 
 const (
-        configFileName string = "./cookies.json"
-        listsFileName  string = "./gameslist.json"
+	cookiesFileName string = "./cookies.json"
+	listsFileName   string = "./gameslist.json"
+	configFileName  string = "./config.json"
 )
 
 const (
-        // name of the service
-        serviceName        = "sgbotservice"
-        serviceDescription = "SteamGifts Bot Service"
+	// name of the service
+	serviceName        = "sgbotservice"
+	serviceDescription = "SteamGifts Bot Service"
 
-        // port which daemon should be listen
-        servicePort = ":9977"
+	// port which daemon should be listen
+	servicePort = ":9977"
 )
 
 // Service has embedded daemon
 type Service struct {
-        daemon.Daemon
+	daemon.Daemon
+
+	bot *TheBot
 }
 
-//      dependencies that are NOT required by the service, but might be used
+//	dependencies that are NOT required by the service, but might be used
 var dependencies = []string{"dummy.service"}
 
 var stdlog, errlog *log.Logger
@@ -39,137 +42,148 @@ var stdlog, errlog *log.Logger
 // Manage by daemon commands or run the daemon
 func (service *Service) Manage() (string, error) {
 
-        usage := "Usage: myservice install | remove | start | stop | status"
+	usage := "Usage: myservice install | remove | start | stop | status"
 
-        // if received any kind of command, do it
-        if len(os.Args) > 1 {
-                command := os.Args[1]
-                switch command {
-                case "install":
-                        return service.Install()
-                case "remove":
-                        return service.Remove()
-                case "start":
-                        return service.Start()
-                case "stop":
-                        return service.Stop()
-                case "status":
-                        return service.Status()
-                default:
-                        return usage, nil
-                }
-        }
+	// if received any kind of command, do it
+	if len(os.Args) > 1 {
+		command := os.Args[1]
+		switch command {
+		case "install":
+			return service.Install()
+		case "remove":
+			return service.Remove()
+		case "start":
+			return service.Start()
+		case "stop":
+			return service.Stop()
+		case "status":
+			return service.Status()
+		default:
+			return usage, nil
+		}
+	}
 
-        // Do something, call your goroutines, etc
-        go startBot(service)
+	// Do something, call your goroutines, etc
+	go startBot(service)
 
-        // Set up channel on which to send signal notifications.
-        // We must use a buffered channel or risk missing the signal
-        // if we're not ready to receive when the signal is sent.
-        interrupt := make(chan os.Signal, 1)
-        signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
 
-        // Set up listener for defined host and port
-        //listener, err := net.Listen("tcp", servicePort)
-        //if err != nil {
-        //              return "Possibly was a problem with the port binding", err
-        //      }
+	// Set up listener for defined host and port
+	//listener, err := net.Listen("tcp", servicePort)
+	//if err != nil {
+	//		return "Possibly was a problem with the port binding", err
+	//	}
 
-        // set up channel on which to send accepted connections
-        //      listen := make(chan net.Conn, 100)
-        //      go acceptConnection(listener, listen)
+	// set up channel on which to send accepted connections
+	//	listen := make(chan net.Conn, 100)
+	//	go acceptConnection(listener, listen)
 
-        // loop work cycle with accept connections or interrupt
-        // by system signal
-        for {
-                select {
-                //              case conn := <-listen:
-                //                      go handleClient(conn)
-                case killSignal := <-interrupt:
-                        stdlog.Println("Got signal:", killSignal)
-                        //stdlog.Println("Stoping listening on ", listener.Addr())
-                        //listener.Close()
-                        if killSignal == os.Interrupt {
-                                return "Daemon was interruped by system signal", nil
-                        }
-                        return "Daemon was killed", nil
-                }
-        }
+	// loop work cycle with accept connections or interrupt
+	// by system signal
+	for {
+		select {
+		//		case conn := <-listen:
+		//			go handleClient(conn)
+		case killSignal := <-interrupt:
+			stdlog.Println("Got signal:", killSignal)
+			//stdlog.Println("Stoping listening on ", listener.Addr())
+			//listener.Close()
+			if service.bot != nil {
+				service.bot.SendPanicMsg("Daemon was interruped by system signal")
+			}
+			if killSignal == os.Interrupt {
+				return "Daemon was interruped by system signal", nil
+			}
+			return "Daemon was killed", nil
+		}
+	}
 
-        // never happen, but need to complete code
-        return usage, nil
+	// never happen, but need to complete code
+	return usage, nil
 }
 
 // Accept a client connection and collect it in a channel
 func acceptConnection(listener net.Listener, listen chan<- net.Conn) {
-        for {
-                conn, err := listener.Accept()
-                if err != nil {
-                        continue
-                }
-                listen <- conn
-        }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		listen <- conn
+	}
 }
 
 func handleClient(client net.Conn) {
-        for {
-                buf := make([]byte, 4096)
-                numbytes, err := client.Read(buf)
-                if numbytes == 0 || err != nil {
-                        return
-                }
-                buf = append(buf, 32, 0x30)
-                client.Write(buf[:numbytes+2])
-        }
+	for {
+		buf := make([]byte, 4096)
+		numbytes, err := client.Read(buf)
+		if numbytes == 0 || err != nil {
+			return
+		}
+		buf = append(buf, 32, 0x30)
+		client.Write(buf[:numbytes+2])
+	}
 }
 
 func init() {
-        stdlog = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds)
-        errlog = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lmicroseconds)
+	stdlog = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds)
+	errlog = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lmicroseconds)
 }
 
 func startBot(srv *Service) {
-        stdlog.Println("bot started")
+	stdlog.Println("bot started")
 
-        defer func() {
-                if srv != nil {
-                        srv.Stop()
-                }
-        } ()
+	defer func() {
+		if srv != nil && srv.Daemon != nil {
+			srv.Stop()
+		}
+	}()
 
-        var bot TheBot
-        err := bot.InitBot(configFileName, listsFileName)
-        if err != nil {
-                errlog.Println("error while initialize bot. stop service", err)
-                return
-        }
+	srv.bot = &TheBot{}
 
-        for {
-                err = bot.Check()
-                if err != nil {
-                        errlog.Println("error during check. stop service", err)
-                        break
-                }
-                time.Sleep(time.Hour)
-        }
+	err := srv.bot.InitBot(configFileName, cookiesFileName, listsFileName)
+	if err != nil {
+		errlog.Println("error while initialize bot. stop service", err)
+		srv.bot.SendPanicMsg(fmt.Sprintf("error while initialize bot. stop service. %v", err))
+		return
+	}
+
+	for {
+		count, err := srv.bot.Check()
+		if err != nil {
+			errlog.Println("error during check. stop service", err)
+			srv.bot.SendPanicMsg(fmt.Sprintf("error during check. stop service. %v", err))
+			break
+		}
+		if count == 0 {
+			count = 1
+		} else {
+			count = 2
+		}
+		time.Sleep(time.Hour * time.Duration(count))
+	}
 }
 
 func main() {
-        if len(os.Args) > 1 && os.Args[1] == "test" {
-                startBot(nil)
-                return
-        }
+	if len(os.Args) > 1 && os.Args[1] == "test" {
+		startBot(&Service{nil, nil})
+		return
+	}
 
-        srv, err := daemon.New(serviceName, serviceDescription)
-        if err != nil {
-                errlog.Println("Error: ", err)
-                os.Exit(1)
-        }
-        service := &Service{srv}
-        status, err := service.Manage()
-        if err != nil {
-                errlog.Println(status, "\nError: ", err)
-                os.Exit(1)
-        }
-        fmt.Println(status)
+	srv, err := daemon.New(serviceName, serviceDescription)
+	if err != nil {
+		errlog.Println("Error: ", err)
+		os.Exit(1)
+	}
+	service := &Service{srv, nil}
+	status, err := service.Manage()
+	if err != nil {
+		errlog.Println(status, "\nError: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(status)
 }
